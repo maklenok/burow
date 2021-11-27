079aa682b566d4bc31055cdfc309287a626e1159
maklenok <st1064@bk.ru>
2021-11-27 16:30:10 +0700

    lolw

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+<main class="px-3">
+  <h1>Сайт Сергея Бабатчикова</h1>
+  <p class="lead">Уроженец Новосибирска, студент ВКИ НГУ</p>
+</main>
+<div class="container">
+
+    <v-simple-table dark>
+      <template v-slot:default>
+        <thead>
+        <tr>
+          <th class="text-left">
+            Время
+          </th>
+          <th class="text-left">
+            Занятие
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr
+            v-for="item in time"
+            :key="item.name"
+        >
+          <td>{{ item.employment }}</td>
+          <td>{{ item.name}}</td>
+        </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+
+</div>
+</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      time: [
+        {
+          name: 'Подъём',
+          employment: '7:30',
+        },
+        {
+          name: 'Завтрак',
+          employment: '7:40',
+        },
+        {
+          name: 'Шкила',
+          employment: '8:00',
+        },
+        {
+          name: 'Уроки',
+          employment: '14:00',
+        },
+        {
+          name: 'Игра в компик',
+          employment: '14:00',
+        },
+        {
+          name: 'Ванна',
+          employment: '20:00',
+        },
+        {
+          name: 'Спать',
+          employment: '21:00',
+        },
+      ],
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
